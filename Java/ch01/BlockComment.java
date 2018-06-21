60cc1cde7d265595b22abc41b1ca8f0b0a073146
sungjun(2) <sky794373@naver.com>
2018-06-21 09:49:41 +0900

    자바 수업

---
+++ Java/ch01/BlockComment.java
@@ -0,0 +1,21 @@
+/*
+작 성 자: 조성준
+작 성 일: 2018년 6월 20일
+목    적: System.out.println 메소드의 기능 테스트
+*/
+
+class BlockComment
+{
+	public static void main(String[] args)
+	{
+		/*다음은 단순한 정수의 출력*/
+		System.out.println(7);		
+		System.out.println(3.15);
+		
+		/*다음은 덧셈 결과의 출력*/
+		System.out.println(3 + 5);
+		System.out.println("3 + 5" + " 의 연산 결과는 8입니다.");
+		System.out.println("3 + 5 = " + 8);
+		System.out.println(3.15 + "는 실수입니다.");
+	}
+}
---
+++ Java/ch01/FirstJavaProgram.java
@@ -0,0 +1,8 @@
+class FirstJavaProgram
+{
+	public static void main(String[] args)
+	{
+		System.out.print("Welcome to Java");
+		System.out.print("First Java Program");
+	}
+}
