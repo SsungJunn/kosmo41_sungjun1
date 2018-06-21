e70491a4d574a5ed2488823049d73c1738796ae1
sungjun(2) <sky794373@naver.com>
2018-06-21 14:40:45 +0900

    자바 수업

---
+++ Java/ch04/src/SCE.java
@@ -0,0 +1,20 @@
+//중요!!
+public class SCE {
+
+	public static void main(String[] args) {
+		int num1 = 0;
+		int num2 = 0;
+		boolean result;
+		
+		result = ((num1 += 10) < 0) && ((num2 += 10) > 0); //num1이 true이므로, num2 연산안함
+		System.out.println("result = " + result);
+		System.out.println("num1 = " + num1);
+		System.out.println("num2 = " + num2 + '\n');
+		
+		result = ((num1 += 10) > 0) || ((num2 += 10) > 0); //num1이 true이므로, num2 연산안함
+		System.out.println("result = " + result);
+		System.out.println("num1 = " + num1);
+		System.out.println("num2 = " + num2);
+	}
+
+}
